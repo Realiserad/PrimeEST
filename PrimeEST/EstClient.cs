ec6aa82f2a48325a543710502f5ea3261135525c
Bastian Fredriksson <bastian@primekey.com>
2020-02-23 15:00:20 +0100

    Add project files.

---
+++ PrimeEST/EstClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeEST
+{
+    class EstClient
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
