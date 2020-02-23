b75687e175da2e57460802e1e873a2ddcc23f827
Bastian Fredriksson <bastian@primekey.com>
2020-02-23 15:54:34 +0100

    Sample code for /cacerts and /simpleenroll using  WebClient.

--- PrimeEST/EstClient.cs
+++ PrimeEST/EstClient.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Prng;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 
 namespace PrimeEST
 {
@@ -10,6 +16,37 @@ namespace PrimeEST
     {
         static void Main(string[] args)
         {
+            using (var webClient = new EstWebClient())
+            {
+                webClient.Proxy = null;
+
+                // cacerts
+                Console.WriteLine("------------------ cacerts ---------------------");
+                var certificateChain = webClient.DownloadString("https://nautilus:8442/.well-known/est/staging/cacerts");
+                Console.WriteLine(certificateChain);
+
+                // simpleenroll
+                Console.WriteLine("------------------ simpleenroll ---------------------");
+                var keyPairGenerator = new RsaKeyPairGenerator();
+                keyPairGenerator.Init(new KeyGenerationParameters(new SecureRandom(new CryptoApiRandomGenerator()), 2048));
+                var keyPair = keyPairGenerator.GenerateKeyPair();
+                var extensions = new Dictionary<DerObjectIdentifier, X509Extension>();
+                var subjectAlternativeName = new GeneralName[] { new GeneralName(GeneralName.DnsName, System.Environment.MachineName ) };
+                extensions.Add(X509Extensions.SubjectAlternativeName, new X509Extension(false, new DerOctetString(new GeneralNames(subjectAlternativeName))));
+                Pkcs10CertificationRequest csr = new Pkcs10CertificationRequest("SHA256withRSA",
+                    new X509Name("CN=Windows Autoenrollment"),
+                    keyPair.Public,
+                    new DerSet(new AttributePkcs(PkcsObjectIdentifiers.Pkcs9AtExtensionRequest, new DerSet(new X509Extensions(extensions)))),
+                    keyPair.Private);
+                string base64Csr = Convert.ToBase64String(csr.GetDerEncoded());
+                webClient.Headers["Content-Type"] = "application/pkcs10";
+                webClient.Headers["Content-Transfer-Encoding"] = "base64";
+                webClient.Credentials = new NetworkCredential("staging", "foo123");
+                var certificate = webClient.UploadString("https://nautilus:8442/.well-known/est/staging/simpleenroll", base64Csr);
+                Console.WriteLine(certificate);
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ PrimeEST/EstWebClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PrimeEST
+{
+    internal class EstWebClient : WebClient
+    {
+        public X509Certificate2 ClientCertificate { private get; set; }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            HttpWebRequest request = (HttpWebRequest)base.GetWebRequest(address);
+            ServicePointManager.ServerCertificateValidationCallback = delegate (object obj, X509Certificate serverCertificate, X509Chain chain, SslPolicyErrors errors)
+            {
+                // TODO Allow everything for now
+                return true;
+            };
+            if (ClientCertificate != null)
+            {
+                request.ClientCertificates.Add(ClientCertificate);
+            }
+            return request;
+        }
+    }
+}
